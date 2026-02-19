763906b951aa6f0b919b486dc94fa5d274516bc5
Firedrago95 <yonghwa95@naver.com>
2026-02-19 19:14:48 +0900

    test: ChatAnalysisKafkaConsumer 예외 처리 및 오프셋 커밋 테스트 추가
    - 메시지 분석 중 예외 발생 시에도 Acknowledgment 호출을 검증하는 테스트 추가
    - Poison Pill 방지를 위한 오프셋 커밋 동작 확인
    - createChatMessage 메서드로 테스트 메시지 생성 로직 간소화

--- engine/src/test/java/io/slice/stream/engine/analyzer/application/ChatAnalysisKafkaConsumerTest.java
+++ engine/src/test/java/io/slice/stream/engine/analyzer/application/ChatAnalysisKafkaConsumerTest.java
@@ -1,6 +1,7 @@
 package io.slice.stream.engine.analyzer.application;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -32,22 +33,42 @@ class ChatAnalysisKafkaConsumerTest {
     ChatAnalysisKafkaConsumer chatAnalysisKafkaConsumer;
 
     @Test
-    void 카프카_메시지를_수신하여_분석단을_호출한다() {
+    void 카프카_메시지를_수신하여_분석단을_호출하고_오프셋을_커밋한다() {
         // given
-        ChatMessage chatMessage = new ChatMessage(
-            MessageType.TEXT,
-            new Author("abcd1", "nickname", null),
-            "안녕하세요",
-            Instant.now(),
-            "abcde1234",
-            Map.of()
-        );
-
+        ChatMessage chatMessage = createChatMessage("안녕하세요");
 
         // when
         chatAnalysisKafkaConsumer.consume(chatMessage, ack);
 
         // then
         verify(chatAggregationService, times(1)).aggregate(any(ChatMessage.class));
+        verify(ack, times(1)).acknowledge();
     }
+
+    @Test
+    void 분석_중_예외가_발생해도_오프셋을_커밋하여_Poison_Pill을_방지한다() {
+        // given
+        ChatMessage chatMessage = createChatMessage("에러 유발 메시지");
+        doThrow(new RuntimeException("분석 엔진 일시 오류"))
+            .when(chatAggregationService).aggregate(any(ChatMessage.class));
+
+        // when
+        chatAnalysisKafkaConsumer.consume(chatMessage, ack);
+
+        // then
+        verify(ack, times(1)).acknowledge();
+    }
+
+    private ChatMessage createChatMessage(String content) {
+        return new ChatMessage(
+            MessageType.TEXT,
+            new Author("abcd1", "nickname", null),
+            content,
+            Instant.now(),
+            "abcde1234",
+            Map.of()
+        );
+    }
+
+
 }
