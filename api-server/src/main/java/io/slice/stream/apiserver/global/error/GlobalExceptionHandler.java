cf68f46ffda19a5efe4438c7a973db96fc4e3846
Firedrago95 <yonghwa95@naver.com>
2026-02-23 22:28:10 +0900

    feat: 글로벌 예외 처리 추가 및 공통 ErrorResponse 생성
    - GlobalExceptionHandler로 전역 예외 처리 클래스 구현
    - MethodArgumentNotValidException, BusinessException, Exception 처리 로직 추가
    - 공통 에러 응답 객체(ErrorResponse) 생성 및 정적 팩토리 메서드 제공
    - 요청 데이터 검증 및 시스템 에러 시 표준화된 응답 형식을 반환
    - 로그 추가로 에러 발생 상황 기록 및 디버깅 용이성 개선

---
+++ api-server/src/main/java/io/slice/stream/apiserver/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package io.slice.stream.apiserver.global.error;
+
+import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("[예외] 요청 데이터 검증 실패: {}건의 필드에서 오류 발생", e.getBindingResult().getErrorCount());
+
+        HashMap<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        ErrorResponse response = ErrorResponse.of(errorCode, errors);
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
+        log.error("[비즈니스 예외] 발생 코드: {}, 상세 메시지: {}", e.getErrorCode().getCode(), e.getMessage());
+
+        final ErrorCode errorCode = e.getErrorCode();
+        final ErrorResponse response = ErrorResponse.of(errorCode, e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("[시스템 오류] 예상치 못한 에러가 발생했습니다.", e);
+
+        final ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        final ErrorResponse response = ErrorResponse.of(errorCode);
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+}
