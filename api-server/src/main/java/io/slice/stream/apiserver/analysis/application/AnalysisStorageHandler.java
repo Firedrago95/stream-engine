2829ad5f41ceb2448ab28feda7a6cda5d5b56b21
Firedrago95 <yonghwa95@naver.com>
2026-03-03 19:05:47 +0900

    refactor: AnalysisCommandService와 StorageHandler 트랜잭션 처리 방식 변경
    - AnalysisCommandService에서 @Transactional 어노테이션 제거
    - AnalysisStorageHandler의 handleAnalysisSignal 메서드에 @Transactional 추가
    - 명시적 트랜잭션 적용으로 서비스 및 저장 로직의 책임 명확화
    - 불필요한 트랜잭션 범위 축소로 코드 최적화

--- api-server/src/main/java/io/slice/stream/apiserver/analysis/application/AnalysisStorageHandler.java
+++ api-server/src/main/java/io/slice/stream/apiserver/analysis/application/AnalysisStorageHandler.java
@@ -2,6 +2,7 @@ package io.slice.stream.apiserver.analysis.application;
 
 import io.slice.stream.apiserver.analysis.domain.AnalysisRepository;
 import io.slice.stream.apiserver.analysis.domain.AnalysisSignal;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -15,6 +16,7 @@ public class AnalysisStorageHandler {
     private final AnalysisRepository analysisRepository;
 
     @EventListener
+    @Transactional
     public void handleAnalysisSignal(AnalysisSignal signal) {
         log.debug("[Storage] 분석 신호 저장 - 스트림 : {}, 상태 : {}", signal.streamId(), signal.status());
         analysisRepository.save(signal);
