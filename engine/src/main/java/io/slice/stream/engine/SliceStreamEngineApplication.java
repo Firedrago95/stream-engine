37713babe952ca7e063c6c91ab614c43a3fbde74
Firedrago95 <yonghwa95@naver.com>
2026-09-03 14:20:21 +0900

    chore: 기본 로깅 타임존 KST(Asia/Seoul) 통일

--- engine/src/main/java/io/slice/stream/engine/SliceStreamEngineApplication.java
+++ engine/src/main/java/io/slice/stream/engine/SliceStreamEngineApplication.java
@@ -1,5 +1,7 @@
 package io.slice.stream.engine;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -11,6 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class SliceStreamEngineApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SliceStreamEngineApplication.class, args);
